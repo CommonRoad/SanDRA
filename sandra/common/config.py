20caf2724715db09b910f8530823537855b72139
Yuanfei <yuanfei.lin@tum.de>
2025-07-10 14:45:46 +0200

    fix config for two options

--- sandra/common/config.py
+++ sandra/common/config.py
@@ -1,25 +1,34 @@
 import os
-from dataclasses import dataclass
+from typing import List
+from dataclasses import dataclass, field
 
 
 @dataclass
+class HighwayEnvConfig:
+    seeds: List[int] = field(default_factory=lambda: [4213])
+    action_input: bool = True
+
+@dataclass
 class SanDRAConfiguration:
-    api_key = os.getenv("OPENAI_API_KEY")
-    model_name = "gpt-4.1"  # "qwen3:14b"
-    use_ollama = False  # True
-    a_lim = 0.1
+    api_key: str = field(default_factory=lambda: os.getenv("OPENAI_API_KEY"))
+    model_name: str = "gpt-4o"  # e.g., "qwen3:14b"
+    use_ollama: bool = False
+    a_lim: float = 0.1
 
-    h = 15  # time horizon of decision-making
-    dt = 0.2
+    h: int = 15  # time horizon of decision-making
+    dt: float = 0.2
 
-    length = 5.0  # 4.5
-    width = 2.0
+    length: float = 5.0
+    width: float = 2.0
 
-    perception_radius = 100
+    perception_radius: float = 100.0
 
-    plot_limits = [-6.36, 79.56, 4.07, 25.65]
+    plot_limits: list = field(default_factory=lambda: [-6.36, 79.56, 4.07, 25.65])
+
+    highway_env: HighwayEnvConfig = field(default_factory=HighwayEnvConfig)
+
 COMMONROAD_REACH_SEMANTIC_ROOT = (
-    "/home/sebastian/Documents/Uni/Sandra/commonroad-reach-semantic"
+    "/home/liny/repairverse/commonroad-reach-semantic"
 )
 PROJECT_ROOT = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
